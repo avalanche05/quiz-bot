3c2527d2fe37e05484ba1b33f4344581b60552c1
Ivan Ilin <buran05@icloud.com>
2022-12-08 16:16:47 +0300

    Create parse.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import parse
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -9,6 +10,12 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Привет! Я бот, который будет давать тебе задачки. Меня сделали как тестовый '
                                     'проект от "Время 31-х"\n\nВыбери на клавиатуре тест, котороый ты хочешь пройти.')
+
+    tests = parse.get_tests()
+    test_names = []
+    for test in tests:
+        test_names.append(test['title'])
+
 
 
 app = ApplicationBuilder().token("5812448710:AAEa6lV4pvXBesAqfBhHRfQU0kNwnlZnErc").build()
